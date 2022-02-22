9895bfeadfd35ddfa0be59798be90795909d1d75
Rutuja Sanjay Jadhav <82172257+rutujaJ2002@users.noreply.github.com>
2022-02-22 16:54:04 +0530

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+}
